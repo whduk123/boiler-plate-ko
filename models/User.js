be460084a2377cf297f2046abc0e8b479a21fdb7
Kimjongwook <whduk123@gmail.com>
2020-03-18 23:16:38 +0900

    비밀 정보 보호

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 const userSchema = mongoose.Schema( {
     name: {
         type : String,
@@ -32,6 +31,6 @@ const userSchema = mongoose.Schema( {
     }
 })
 
-conset User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
